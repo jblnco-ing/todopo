977b31b92312ad9a08143fdff966cc352a0bf133
Joel Blanco <joeldavidblancomedina@gmail.com>
2019-08-23 07:06:19 -0400

    todo esta listo, falta usar el local storage y agregar l pomodoro

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ class Task{
 		this.isComplete = false;
 	}
 
-	completed() {
+	complete() {
 		this.isComplete = !this.isComplete;
 	}
 }
@@ -14,27 +14,42 @@ class TaskList{
 		this.name = name;
 		this.element = element;
 		this.tasks = [];
+		// this.tasks = this.loadTasks();
+		// this.renderTasks();
 	}
 
 	addTask(task) {
 		this.tasks.push(task);
+		// this.saveTasks(this.tasks);
 		this.renderTasks();
 	}
 
 	removeTask(i) {
 		this.tasks.splice(i,1);
+		// this.saveTasks(this.tasks);
 		this.renderTasks();
 	}
 
 	renderTasks() {
+
 		let tasks = this.tasks.map((task) => `
 			<li class="task">
-				<input type="checkbox" class="task__complete-button">
+				<input type="checkbox" ${task.isComplete? 'checked' : ''} class="task__complete-button">
 				<span class="task__name">${task.name}</span>
 				<a href="#" class="task__remove-button">X</a>
 			</li>
 		`);
-		this.element.innerHTML = tasks.reduce((a,b) => a + b);
+		this.element.innerHTML = tasks.reduce((a,b) => a + b, '');
+	}
+
+	saveTasks(tasks){
+		localStorage.setItem("@todopo:tasks", tasks);
+		// localStorage.setItem("@todopo:tasks", JSON.stringify(tasks));
+	}
+
+	loadTasks(tasks){
+		return localStorage.getItem("@todopo:tasks");
+		// localStorage.getItem("@todopo:tasks", JSON.stringify(tasks));
 	}
 }
 
@@ -54,17 +69,25 @@ function addDOMTask(e){
 }
 addTaskElement.addEventListener('keyup', addDOMTask);
 
+function getIndexTask(e) {
+	let taskItem = e.target.parentElement,
+		tasksItems = [...inbox.element.querySelectorAll('li')];
+	return tasksItems.indexOf(taskItem);
+}
+
 // Eliminar tareas desde el DOM
 function removeDOMTask(e){
 	if (e.target.tagName === 'A') {
-		console.dir('e');
-
+		inbox.removeTask(getIndexTask(e));
 	}
-	// if (e.key === 'Enter') {
-	// 	const task = new Task(this.value);
-	// 	inbox.addTask(task);
-	// 	this.value = null;
-	// 	console.dir(this);
-	// }
 }
 tasksContainerElement.addEventListener('click', removeDOMTask);
+
+// Completar tareas desde el DOM
+function completeDOMTask(e){
+	if (e.target.tagName === 'INPUT') {
+		inbox.tasks[getIndexTask(e)].complete();
+	}
+		console.table(inbox.tasks);
+}
+tasksContainerElement.addEventListener('click', completeDOMTask);
